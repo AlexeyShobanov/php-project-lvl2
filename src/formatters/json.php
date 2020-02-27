7c50d69698f5c7bcd89546130baee3ea10aa76fc
AlexeyShobanov <shobanov@gmail.com>
2020-02-27 17:26:30 +0300

    fix json.php new backup

--- src/formatters/json.php
+++ src/formatters/json.php
@@ -4,34 +4,19 @@ namespace Alshad\Gendiff\Formatters\Json;
 
 function flattenAst ($ast)
 {
-//print_r("AST!!!!!!!!\n");
-//print_r($ast);
     $flattenAst = function ($ast, $root) use (&$flattenAst) {
-        //print_r("AST!!!!!!!!\n");
-//print_r($ast);
         $result = array_reduce($ast, function($acc, $node) use (&$flattenAst, $root) {
-        //print_r("NODE!!!!!!!!!!!!!\n");
-        //print_r($node);
             if (array_key_exists('children', $node)) {
-                $a = $flattenAst($node['children'], $node);
-        print_r("A!!!!!!!!!!!!!\n");
-        print_r($a);
-                return array_merge($acc, $a);
+                $flatChildrenAst = $flattenAst($node['children'], $node);
+                return array_merge($acc, $flatChildrenAst);
             }
             if ($root) {
-                //print_r("ROOT!!!!!!!!!!!!!\n");
-                //print_r($root);
-                //print_r("\n");
                 $type = $root['type'] === 'unchanged' ? $node['type'] : $root['type'];
-                return array_merge($acc, ['type' => $type, 'key' => $root['key'], 'value' => $node]);
-                /*if ($root['type'] !== 'unchanged') {
-                    $nodeWithParent = array_merge($root, ['value' => $node]);
-                    return array_merge($acc, [$nodeWithParent]); 
-                }
-                $nodeWithParent = array_merge($root, ['value' => $node, 'type' => $node['type']]);
-                return array_merge($acc, [$nodeWithParent]); */
+                $acc[] = ['type' => $type, 'key' => $root['key'], 'value' => $node];
+            } else {
+                $acc[] = $node;
             }
-            return array_merge($acc, [$node]);
+            return $acc;
         }, []);
 
         return $result;
@@ -40,22 +25,48 @@ function flattenAst ($ast)
     return $flattenAst($ast, '');
 }
 
-function renderAstForJsonFormat($ast)
+function flattenValueNode ($node)
 {
-    $flatAst = flattenAst($ast);
-    $renderAst = function ($flatAst, $root) use (&$renderAst) {
-        print_r($flatAst);
-        print_r("\n");
-        $jsonRepresentationOfNodes = array_reduce($flatAst, function ($acc, $node) use ($root, &$renderAst) {
-            ['type' => $type, 'key' => $key, 'value' => $value] = $node;
+    $flattenValueNode = function ($node) use (&$flattenValueNode) {
+        ['key' => $key, 'value' => $value] = $node;
+        if (is_array($value)) {
+            $b = $flattenValueNode($value);
+                $modifiedValue = $b;
+        } else {
             $modifiedValue = !is_bool($value) ? $value : ($value === true ? 'true' : 'false');
-            if (isset($node['oldValue'])) {
+            if ($node['oldValue']) {
                 $oldValue = $node['oldValue'];
                 $modifiedOldValue = !is_bool($oldValue) ? $oldValue : ($oldValue === true ? 'true' : 'false');
+                $modifiedValue = ["new" => $modifiedValue, "old" => $modifiedOldValue];
             }
-            return array_merge($acc, $type === 'changed' ?
-                ["{$type}" => ["{$key}" => ["new" => "{$modifiedValue}", "old" => "{$modifiedOldValue}"]]] :
-                ["{$type}" => ["{$key}" => "{$modifiedValue}"]]);
+        }
+        $flatValue = [$key => $modifiedValue];
+
+        return $flatValue;
+    };
+
+    return $flattenValueNode($node);
+}
+
+function renderAstForJsonFormat($ast)
+{
+    //$flatAst = flattenAst($ast);
+    $flatAst = $ast;
+    $renderAst = function ($flatAst) use (&$renderAst) {
+        print_r($flatAst);
+        print_r("\n");
+        $jsonRepresentationOfNodes = array_reduce($flatAst, function ($acc, $node) use (&$renderAst) {
+            ['type' => $type, 'key' => $key, 'value' => $value] = $node;
+            $a = flattenValueNode($node);
+            print_r("!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            print_r($a);
+        print_r("\n");
+        $acc[$type][] = $a;
+        //$b = array_merge($acc, [$type => $a]);
+        //print_r("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+          //  print_r($acc);
+        //print_r("\n");
+            return $acc;
         },
             ['added' => [], 'removed' => [], 'changed' => [], 'unchanged' => []]
         );
